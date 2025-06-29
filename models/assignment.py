08cc35ad530276b821d3e944c42b05d4cb8fafc9
spcameron <cameron.seanpatrick@gmail.com>
2025-06-28 20:56:30 -0400

    Stubbing out Assignments

--- models/assignment.py
+++ models/assignment.py
@@ -3,5 +3,28 @@
 
 class Assignment:
 
-    def __init__(self):
-        pass
+    def __init__(self, id: str, name: str, category_id: str, points_possible: float, is_extra_credit: bool=False):
+        self.id = id
+        self.name = name
+        self.category_id = category_id
+        self.points_possible = points_possible
+        self.is_extra_credit = is_extra_credit
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "category_id": self.category_id,
+            "points_possible": self.points_possible,
+            "extra_credit": self.is_extra_credit,
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Assignment":
+        return cls (
+            id=data["id"],
+            name=data["name"],
+            category_id=data["category_id"],
+            points_possible=data["points_possible"],
+            is_extra_credit=data["extra_credit"],
+        )
